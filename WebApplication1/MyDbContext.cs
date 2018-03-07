3582dbba38aa9f615c526b2192ebb50757935121
guguohai <guohai@live.com>
2018-03-07 17:46:40 +0800

    添加项目文件。

---
+++ WebApplication1/MyDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static WebApplication1.Models.AppModel;
+
+namespace WebApplication1
+{
+    public class MyDbContext : DbContext
+
+    {
+
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+
+        : base(options)
+
+        { }
+
+
+        public DbSet<Blog> Blogs { get; set; }
+
+        public DbSet<Post> Posts { get; set; }
+
+    }
+
+
+}
